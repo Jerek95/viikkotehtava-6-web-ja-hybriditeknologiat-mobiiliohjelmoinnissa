a9e8ba3e0254fa18ff80db65d7cc9d9d80ffd2c0
Jere Koivuranta <jere.koivuranta@gmail.com>
2026-02-16 16:20:21 +0200

    alustettu

---
+++ barcode/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+
+export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  
+  return (
+    <View style={styles.container}>
+      <Text>Scan barcode</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
